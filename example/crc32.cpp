5d4880efd7cab7c802fbcfe4cdae61b583c4c4de
Raul Ramos <igaltza@gmail.com>
2020-01-22 19:14:04 +0000

    refactor sync "cout" class for more convenient name

--- example/crc32.cpp
+++ example/crc32.cpp
@@ -117,11 +117,11 @@ auto time_now () -> uint64_t {
 }
 
 // Non-interleaving debug print
-class println {
+class internal_coutln {
 
  public:
 
-    println() {
+    internal_coutln() {
         auto elapsed = high_resolution_clock::now() - g_start_time;
         cout << "[" << dec << duration_cast<nanoseconds>(elapsed).count() << "]";
         if (auto it = g_tid2str.find(this_thread::get_id()); it != g_tid2str.end()) {
@@ -131,18 +131,20 @@ class println {
     }
 
     template <typename T> 
-    auto operator<<(T const& _val) -> println& {
+    auto operator<<(T const& _val) -> internal_coutln& {
        stream_ << _val;
        return *this;
     }
 
-    ~println() {
+    ~internal_coutln() {
        cout << stream_.str() << endl;
     }
 
  private:
     ostringstream stream_;
 };
+
+#define coutln internal_coutln()
 
 constexpr auto operator""_KiB(uint64_t _v) -> uint64_t {
     return 1024u * _v;
@@ -170,7 +172,7 @@ void producer () {
         lock_guard<mutex> lock(g_print_mutex);
         g_tid2str[this_thread::get_id()] = string("Producer");
     }
-    println() << "Starting";
+    coutln << "Starting";
 
     mt19937 gen(random_device{}());
     uniform_int_distribution<> dis(1, g_rbuffer.capacity() - 1);
@@ -203,7 +205,7 @@ void producer () {
     }
 
     auto ns = duration_cast<nanoseconds>(high_resolution_clock::now() - t0).count();
-    println() << "Transfer speed = " << dec << (((double)g_data_size / 1_MiB) / (ns / 1000000000.0)) << " MiB/sec";
+    coutln << "Transfer speed = " << dec << (((double)g_data_size / 1_MiB) / (ns / 1000000000.0)) << " MiB/sec";
 }
 
 /*
@@ -217,7 +219,7 @@ void consumer() {
         lock_guard<mutex> lock(g_print_mutex);
         g_tid2str[this_thread::get_id()] = string("Consumer");
     }
-    println() << "Starting";
+    coutln << "Starting";
 
     const auto process_chunk = [&](auto _buff, auto _len) { 
         g_consumer_hash = crc32(_buff, _len, g_consumer_hash); 
@@ -243,7 +245,7 @@ void consumer() {
     g_consumer_hash = g_consumer_hash ^ 0xFFffFFff;
 
     auto ns = duration_cast<nanoseconds>(high_resolution_clock::now() - t0).count();
-    println() << "Read/process speed = " << dec << (((double)g_data_size / 1_MiB) / (ns / 1000000000.0)) << " MiB/sec";
+    coutln << "Read/process speed = " << dec << (((double)g_data_size / 1_MiB) / (ns / 1000000000.0)) << " MiB/sec";
 }
 
 /*
@@ -260,11 +262,11 @@ auto main() -> int {
     g_data_size = 420_MiB;
     g_data = make_unique<uint8_t[]>((size_t)g_data_size);
     if (!g_data) {
-        println() << "ERR: No memory!";
+        coutln << "ERR: No memory!";
         return 1;
     }
 
-    println() << "Generating random sample of " << (float)g_data_size / 1_MiB << " MiB...";
+    coutln << "Generating random sample of " << (float)g_data_size / 1_MiB << " MiB...";
     auto step = g_data_size / thread::hardware_concurrency();
     auto start = g_data.get();
     auto end = start + g_data_size;
@@ -292,19 +294,19 @@ auto main() -> int {
 
     // Calculate the crc32
 
-    println() << "Calculating crc32...";
+    coutln << "Calculating crc32...";
     g_producer_hash = crc32(g_data.get(), g_data_size, g_producer_hash);
     g_producer_hash = g_producer_hash ^ 0xFFffFFff;
-    println() << "Crc32 = 0x" << hex << g_producer_hash;
+    coutln << "Crc32 = 0x" << hex << g_producer_hash;
 
     // Reserve space for the ring buffer
 
-    println() << "Creating buffer...";
+    coutln << "Creating buffer...";
     if (!g_rbuffer.reserve((uint32_t)2_MiB)) {
-        println() << "ERR: No memory!";
+        coutln << "ERR: No memory!";
         return 1;
     }
-    println() << "Buffer Capacity = " << (float)g_rbuffer.capacity() / 1_MiB << " MiB";
+    coutln << "Buffer Capacity = " << (float)g_rbuffer.capacity() / 1_MiB << " MiB";
 
     // Run threads
 
@@ -314,11 +316,11 @@ auto main() -> int {
     producer_thread.join();
     consumer_thread.join();
     auto ns = duration_cast<nanoseconds>(high_resolution_clock::now() - t0).count();
-    println() << (g_consumer_hash == g_producer_hash ? "PASSED" : "ERROR") << " (crc32 == 0x" << hex << g_consumer_hash << ")";
-    println() << "== Stats == ";
-    println() << "Number of times the producer could not write = " << dec << g_failed_writes;
-    println() << "Number of times the consumer could not read  = " << dec << g_failed_reads;
-    println() << "Time elapsed  = " << dec << ns << " ns, " << ns / 1000.f << " us, " << ns / 1000000.f << " ms, " << ns / 1000000000.f << " sec";
+    coutln << (g_consumer_hash == g_producer_hash ? "PASSED" : "ERROR") << " (crc32 == 0x" << hex << g_consumer_hash << ")";
+    coutln << "== Stats == ";
+    coutln << "Number of times the producer could not write = " << dec << g_failed_writes;
+    coutln << "Number of times the consumer could not read  = " << dec << g_failed_reads;
+    coutln << "Time elapsed  = " << dec << ns << " ns, " << ns / 1000.f << " us, " << ns / 1000000.f << " ms, " << ns / 1000000000.f << " sec";
 
     return 0;
 }
